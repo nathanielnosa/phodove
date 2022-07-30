5cd3dd1d476d4a7597affff19ea6b9b083076266
Nathaniel NOsa <proncode@gmail.com>
2022-07-30 11:19:10 +0100

    first code

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+let swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+      }
+  });
+
+
+  AOS.init();
